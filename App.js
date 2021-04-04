0a6116fab897363dac4c9b973c4b860fac06559c
Marco Guzman <62027255+Marcoguzman211@users.noreply.github.com>
2021-04-04 09:34:15 +0200

    First commit before states

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search'
+
+export default function App() {
+  return (
+    <Search />
+  );
+}
